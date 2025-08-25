037cbdb2ee3fd85bb48b47d3bc7ba0dca97394f9
Welp Development <noreply@welp.com>
2025-08-24 18:55:14 -0700

    Configure custom domain mywelp.com for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/welp-customer-insights/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
